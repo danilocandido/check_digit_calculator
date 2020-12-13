05fc3bba5cc0d288462b9846d0e912f2e094aade
Danilo Cândido <danilod07@gmail.com>
2020-12-13 11:36:01 -0300

    Refactor ruby code

--- lib/calculate_check_digit.rb
+++ lib/calculate_check_digit.rb
@@ -1,15 +1,19 @@
 # frozen_string_literal: true
 
-# !/usr/bin/env ruby
-
 class CalculateCheckDigit
+  WEIGHTS = [8, 7, 6, 5, 4, 3, 2, 10, 0, 9, 8, 7, 6, 5, 4, 3, 2].freeze
+  MAP_DIGITS = %w[0 1 2 3 4 5 6 7 9 10 X].freeze
   CHECK_DIGIT_INDEX = 8
+
+  attr_reader :vin
 
   def self.[](vin)
     new(String(vin)).response
   end
 
   def response
+    return unless vin
+
     <<~TXT
       Provided VIN: #{vin}
       Check Digit: #{result}
@@ -19,19 +23,20 @@ class CalculateCheckDigit
 
   private
 
-  attr_reader :vin
 
   def initialize(vin)
-    @vin = vin
+    @vin = vin.strip
   end
 
   def check_digit
+    raise CheckDigitInvalid if invalid_size?
+
     vin
       .chars
       .each_with_index
-      .reduce(0) { |acc, (char, i)| acc + transliterate(char) * weights[i] }
-      .yield_self { |sum| map[sum % 11] }
-  rescue StandardError
+      .reduce(0) { |acc, (char, i)| acc + transliterate(char) * WEIGHTS[i] }
+      .yield_self { |sum| MAP_DIGITS[sum % 11] }
+  rescue StandardError, CheckDigitInvalid
     nil
   end
 
@@ -39,16 +44,14 @@ class CalculateCheckDigit
     '0123456789.ABCDEFGH..JKLMN.P.R..STUVWXYZ'.index(char) % 10
   end
 
-  def map
-    %w[0 1 2 3 4 5 6 7 9 10 X]
-  end
-
-  def weights
-    [8, 7, 6, 5, 4, 3, 2, 10, 0, 9, 8, 7, 6, 5, 4, 3, 2]
-  end
-
   def valid_check_digit?
     vin[CHECK_DIGIT_INDEX] == check_digit
+  end
+
+  def invalid_size?
+    return if vin.empty?
+
+    vin.size != 17
   end
 
   def result
@@ -57,8 +60,7 @@ class CalculateCheckDigit
 
   def message
     case result
-    when :VALID
-      'This looks like a VALID vin!'
+    when :VALID then 'This looks like a VALID vin!'
     when :INVALID
       <<~TXT
         Suggested VIN(s):
@@ -66,4 +68,6 @@ class CalculateCheckDigit
       TXT
     end
   end
+
+  class CheckDigitInvalid < StandardError; end
 end
