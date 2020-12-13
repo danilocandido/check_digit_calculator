87a09bbbff94540d298cd54388c92acfa0857919
Danilo Cândido <danilod07@gmail.com>
2020-12-13 11:36:31 -0300

    Include parent describe method

--- spec/calculate_check_digit_spec.rb
+++ spec/calculate_check_digit_spec.rb
@@ -5,16 +5,25 @@ require_relative '../lib/calculate_check_digit'
 describe CalculateCheckDigit do
   subject { CalculateCheckDigit[vin] }
 
-  context 'valid' do
-    let(:vin) { '2NKWL00X16M149834' }
+  describe '#response' do
+    describe 'valid' do
+      let(:vin) { '2NKWL00X16M149834' }
 
-    it { should include('This looks like a VALID vin!') }
-  end
+      it { should include('Check Digit: VALID') }
+      it { should include('This looks like a VALID vin!') }
 
-  context 'wrong formats' do
-    [nil, ''].each do |value|
-      let(:vin) { value }
-      it { should include('INVALID') }
+      context 'check digit X' do
+        let(:vin) { '1M8GDM9AXKP042788' }
+
+        it { should include('Check Digit: VALID') }
+      end
+    end
+
+    describe 'invalid format' do
+      [nil, '', ' '].each do |value|
+        let(:vin) { value }
+        it { should include('INVALID') }
+      end
     end
   end
 end
