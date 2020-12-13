35dedb5db666726ba0b428371cd9157ccc9bcc19
Danilo Cândido <danilod07@gmail.com>
2020-12-12 22:31:18 -0300

    Add result message method

--- lib/calculate_check_digit.rb
+++ lib/calculate_check_digit.rb
@@ -8,18 +8,34 @@ vin = ARGV[0]
 
 class CalculateCheckDigit
   def self.[](vin)
-    new[vin]
+    new(vin).response
   end
 
-  def [](vin)
+  def response
+    <<~TXT
+      Provided VIN: #{vin}
+      Check Digit: #{check_digit} #{result}
+      #{message}
+    TXT
+  end
+
+  private
+
+  attr_reader :vin
+
+  def initialize(vin)
+    @vin = vin
+  end
+
+  def check_digit
     vin
       .chars
       .each_with_index
       .reduce(0) { |acc, (char, i)| acc + transliterate(char) * weights[i] }
       .yield_self { |sum| map[sum % 11] }
+  rescue StandardError
+    nil
   end
-
-  private
 
   def transliterate(char)
     '0123456789.ABCDEFGH..JKLMN.P.R..STUVWXYZ'.index(char) % 10
@@ -31,6 +47,17 @@ class CalculateCheckDigit
 
   def weights
     [8, 7, 6, 5, 4, 3, 2, 10, 0, 9, 8, 7, 6, 5, 4, 3, 2]
+  end
+
+  def message
+    {
+      VALID: 'This looks like a VALID vin!',
+      INVALID: 'Suggested VIN(s):'
+    }[result]
+  end
+
+  def result
+    check_digit ? :VALID : :INVALID
   end
 end
 
